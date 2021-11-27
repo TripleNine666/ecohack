c48f58122a6acdfe3dffbf996c939dcb83731465
TripleNine666 <kolumb1709@gmail.com>
2021-11-27 11:56:19 +0300

    Pymongo_Methods v2

--- Kirill_Kolubovich/Pymongo_Methods.py
+++ Kirill_Kolubovich/Pymongo_Methods.py
@@ -9,48 +9,73 @@ app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'Maindb'
 app.config['MONGO_URI'] = \
-  'mongodb+srv://TripleNine:oBuQDEujor84HtKW@cluster0.trbei.mongodb.net/Maindb?retryWrites=true&w=majority'
+  'mongodb+srv://admin:cAUm4f8cffJuucQ@ecocluster.3lana.mongodb.net/maindb?retryWrites=true&w=majority'
 mongo = PyMongo(app)
+permitted = ["type", "name", "district", "description", "latitude", "longitude", "phone", "site", "image"]
 
 
-@app.route('/agro', methods=['GET'])
-def get_all_stars():
+@app.route('/getall', methods=['GET'])
+def get_all_items():
   agro = mongo.db.AgroUsad
   output = []
   for s in agro.find():
-    output.append({
-      'name': s['name'],
-      'district': s['district'],
-      'address': s['address']
-    })
+    out = {}
+    out.update((key, val) for (key, val) in s.items() if key in permitted)
+    output.append(out)
   return jsonify({'result': output})
 
 
-@app.route('/agro/', methods=['GET'])
-def get_one_star(name):
+@app.route("/")
+def home_view():
+  return "<h1>Welcome to Eco Backend</h1>"
+
+
+@app.route('/getbyname/', methods=['POST'])
+def get_one_by_name():
   agro = mongo.db.AgroUsad
-  s = agro.find_one({'name': name})
+  s = agro.find_one({'name': request.json['name']})
   if s:
-    output = {
-      'name': s['name'],
-      'district': s['district'],
-      'address': s['address']
-    }
+    out = {}
+    out.update((key, val) for (key, val) in s.items() if key in permitted)
+    return jsonify({'result': out})
   else:
     output = "No such name"
   return jsonify({'result': output})
 
 
-@app.route('/agro', methods=['POST'])
-def add_star():
+@app.route('/getbytype/', methods=['POST'])
+def get_one_by_type():
   agro = mongo.db.AgroUsad
-  name = request.json['name']
-  district = request.json['district']
-  agro_id = agro.insert({'name': name, 'district': district})
-  new_agro = agro.find_one({'_id': agro_id})
-  output = {'name': new_agro['name'], 'district': new_agro['district']}
+  s = agro.find_one({'type': request.json['type']})
+  if s:
+    out = {}
+    out.update((key, val) for (key, val) in s.items() if key in permitted)
+    return jsonify({'result': out})
+  else:
+    output = "No such name"
   return jsonify({'result': output})
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/addnew', methods=['POST'])
+def add_new():
+  agro = mongo.db.AgroUsad
+  print(request.get_json())
+  agro_id = agro.insert(request.get_json())
+  new_agro = agro.find_one({'_id': agro_id})
+  return jsonify({'result': "success"})
+
+
+# delete
+@app.route('/deletebyname', methods=['POST'])
+def delete_one():
+  agro = mongo.db.AgroUsad
+  s = agro.find_one({'name': request.json['name']})
+  if s:
+    agro.delete_one(s)
+    return jsonify({'result': "success"})
+  else:
+    output = "No such name"
+  return jsonify({'result': output})
+
+
+# edit
